fe4095080a038b700d10eab33e6eeea9cd8585b8
rodemore <rodemore@espol.edu.ec>
2020-04-02 20:55:40 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+
+
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const cors = require('cors');
+const config = require('./config');
+const socketIO = require('socket.io');
+const bodyParser = require('body-parser')
+
+
+
+
+// settings
+app.set('port', process.env.PORT || 3000)
+app.set('json spaces', 2);
+// middlewarese
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: true, limit: '1000mb'}));
+
+//app.use(express.json());
+//app.use(bodyParser.json()); 
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json({limit: '1000mb', extended: true}))
+
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({limit: '200mb', extended: true, parameterLimit: 1000000}))
+//app.use(express.bodyParser({limit: '50mb'}))
+
+app.use(cors(
+  config.application.cors.server
+));
+// routes
+app.use(require('./routes/index'));
+app.use("/api/data",require('./routes/data'));
+
+
+
+// starting the server
+const server = app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
+
+  
+
+//websockets
+ 
+const io = socketIO.listen(server);
+
+io.on('connection', (socket)=>{
+  console.log("new connection")
+  console.log("coneccion ",socket.id)
+ 
+
+
+  socket.on("finish_game", (data) => {
+    console.log("JUEGO TERMINADO");
+    console.log(data)
+    io.sockets.emit("finish_game_board", data);
+
+  });
+
+  socket.on("send_data", (data) => {
+    console.log(data)
+    io.sockets.emit("send", data);
+
+  });
+
+  socket.on("data", (data) => {
+    console.log("recibo data");
+    io.sockets.emit("save_data",data)
+
+
+  });
+  socket.on("start_game", (data)=>{
+    console.log("hey x2")
+    io.sockets.emit("start_game",data)
+  });
+
+
+
+
+
+
+});
---
+++ src/routes/data.js
@@ -0,0 +1,50 @@
+const { Router } = require('express');
+let fs = require("fs");
+const router = Router();
+
+//console.log(pos)
+
+
+router.get('/', (req,res) =>{
+    res.json(pos)
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    
+    var dir = `Experimentos/${req.body["id"]}`;
+
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    let index = 0
+
+    var filename = "hola.json"
+
+        
+    var json = JSON.stringify(req.body["json_data"]);
+
+    fs.writeFile(dir+"/"+filename, json, {}, () => {
+    });
+    
+ 
+    fs.readdir("Experimentos/respaldo", function (err, archivos) {
+        if (err) {
+            console.log("ERROR")
+            onError(err);
+            return;
+        }   
+        index = archivos.length
+        fs.writeFile(fi, json, {}, () => {
+        });
+    });
+
+
+
+    });
+
+
+
+
+module.exports = router;
