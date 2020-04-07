b8a6b9b44e59f1b74ca9c9f26dbac913b7caf3da
rodemore <rodemore@espol.edu.ec>
2020-04-06 21:37:35 -0500

    configuracion cors

--- src/index.js
+++ src/index.js
@@ -22,11 +22,12 @@ const bodyParser = require('body-parser')
 ));
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "*");
-   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-   next();
-}) 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 //app.use(cors());
 app.options('*', cors());
 
