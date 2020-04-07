c66080a64a95d8a788a034e1e63ee13a685d8497
rodemore <rodemore@espol.edu.ec>
2020-04-06 21:12:02 -0500

    configuracion cors

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const bodyParser = require('body-parser')
 
 app.use((req, res, next) => {
    res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Headers", "*");
    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
    next();
 }) 
